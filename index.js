058a1f728250c18e3a4d692b515d7ee9b6ddf34d
brad <bradmedeiros0@gmail.com>
2018-04-12 01:14:05 -0700

    fetch software list to populate the ui for real

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 
 const express  = require('express');
 const path = require('path');
+const software = require('./src/software');
 
 const HTML_FILE = path.resolve('./index.html');
 
 const app = express();
 
-app.get('*', (req, res) => {
+app.get('/software', (req, res) => {
+   res.jsonp(software.getAvailableSoftware());
+});
+app.post('/software/:dockerUrl', async (req, res) => {
+   const dockerUrl = req.params.dockerUrl;
+   try {
+       await software.setSoftwareAsActive(dockerUrl);
+       res.send('ok');
+   }catch(e){
+       res.status(500).jsonp({ error: 'internal server error' })
+   }
+});
+
+app.get('/', (req, res) => {
     res.sendFile(HTML_FILE)
+});
+app.get('/style.css', (req, res) => {
+    res.sendFile(path.resolve('./style.css'));
 });
 
 const PORT = 9000;
